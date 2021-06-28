2a93d59d47b4eea512a186799ce7206380e01415
Tyler <tyler@webguyty.com>
2021-06-28 16:18:56 -0700

    checking for falsy values

--- functions.js
+++ functions.js
@@ -2,6 +2,10 @@ const functions = {
   add: (num1, num2) => {
     return num1 + num2;
   },
+
+  isNull: () => null,
+
+  checkValue: x => x,
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,18 @@ const functions = require('./functions');
 test('Adds 2 + 2 to equal 4', () => {
   expect(functions.add(2, 2)).toBe(4);
 });
+
+test('Adds 2 + 2 to NOT equal 5', () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// Check for null
+
+test('Should be null', () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+// Check for falsy value
+test('Should be falsy', () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+});
